8563109844fe8b8228d8de415c0c763420c6aa25
ALI.JS <aliismailh08@gmail.com>
2023-03-16 06:49:39 -0700

    print the size of data types in diffrence OS

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,9 +7,13 @@
 int main(void)
 {
 printf("Size of a char: %lu byte(s)", sizeof(char));
+printf("\n");
 printf("Size of a int: %lu byte(s)", sizeof(int));
+printf("\n");
 printf("Size of a long int: %lu byte(s)", sizeof(long int));
+printf("\n");
 printf("Size of a long long int: %lu byte(s)", sizeof(long long int));
+printf("\n");
 printf("Size of a float: %lu byte(s)", sizeof(float));
 return (0);
 }
