cb5abb7c37b140c400de52977d065f9b6da51709
ALI.JS <aliismailh08@gmail.com>
2023-03-16 08:04:17 -0700

    print string without puts and printf

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<unistd.h>
+
+/**
+ * main - is the main function
+ * Return: return 1
+ */
+
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stdout);
+return (1);
+}
