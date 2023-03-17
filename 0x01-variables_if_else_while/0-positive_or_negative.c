4b4fa12f5f9c151a9308bb97dfea8c49f612caa3
ALI.JS <aliismailh08@gmail.com>
2023-03-17 11:37:30 -0700

    readme file

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,4 +1,6 @@
 #include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
  * main - is the main function
  * Return: always 0
@@ -10,15 +12,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("%d is positive");
+printf("%d is positive", n);
 }
 else if (n == 0)
 {
-printf("%d is zero");
+printf("%d is zero", n);
 }
 else
 {
-printf("%d is negative");
+printf("%d is negative", n);
 }	
 
 return (0);
