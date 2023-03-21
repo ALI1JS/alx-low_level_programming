1a181272f3c520ec73357aaa4b06fadcfb1d69d9
ALI.JS <aliismailh08@gmail.com>
2023-03-21 11:41:16 -0700

    print alphabet 10 time in lowercase

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphbet_x10 - print alphabet 10 time in lowercase
+ */
+
+void print_alphabet_x10(void)
+{
+int i;
+for (i = 0; i <= 10; i++)
+{
+print_alphabet();
+}
+}
