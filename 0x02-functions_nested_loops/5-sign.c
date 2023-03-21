24379cb90c799f33c0328fcbbfab71f48b373a69
ALI.JS <aliismailh08@gmail.com>
2023-03-21 13:34:45 -0700

    sign function

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_sign - is a function sign checker
+ * @n: is the parameter for checker
+ * Return:1 or 0 or -1
+ */
+
+int print_sign(int n)
+{
+if (n > 0)
+{
+return (1);
+}
+else if (n == 0)
+{
+return (0);
+}
+return (-1);
+}
