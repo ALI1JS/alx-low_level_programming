af317cf432cccd0cfc073a55dabdaf2e68d412e2
ALI.JS <aliismailh08@gmail.com>
2023-03-21 14:04:56 -0700

    this function for return the last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_last_digit - is function for find the last digit
+ * @n: is a parameter for tjis function
+ * Return:d
+ */
+int print_last_digit(int n)
+{
+int d = n % 10;
+return (d);
+}
