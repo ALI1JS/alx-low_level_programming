62d34716af0c6e697606342a2004bd72f8589fa6
ALI.JS <aliismailh08@gmail.com>
2023-03-21 13:53:20 -0700

    absolute function

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - is absolute function
+ * Description - this function return the absolute value for its paramter
+ * @n: is the parameter for return its absolute vlaue
+ * Return: @n
+ */
+
+int _abs(int n)
+{
+if (n >= 0)
+{
+return (n);
+}
+return (0 - (n));
+}
