0bd68888ee1d4044954fa280e41c7121555fd97a
ALI.JS <aliismailh08@gmail.com>
2023-06-23 02:22:39 -0700

    betty style

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- *  * _isupper - uppercase letters
- *   * @c: char to check
- *    *
- *     * Return: 0 or 1
- *      */
+ *   _isupper - uppercase letters
+ *  @c: char to check
+ *  Return: 0 or 1
+ */
 
 int _isupper(int c)
 {
@@ -14,4 +13,3 @@ return (1);
 else
 return (0);
 }
-
