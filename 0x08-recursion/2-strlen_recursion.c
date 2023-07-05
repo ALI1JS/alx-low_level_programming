4be4e00c50854f7fc9c7fc496aa6addbd7801b9e
ALI.JS <aliismailh08@gmail.com>
2023-07-05 16:29:45 -0700

    last version from recursion tasks

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - function to return the lenght of string in recursion
+ * @s : is a string parameter
+ * Return: len
+ */
+
+
+int _strlen_recursion(char *s)
+{
+int longit = 0;
+
+if (*s)
+{
+longit++;
+longit += _strlen_recursion(s + 1);
+}
+
+return (longit);
+
+}
